45ce0c8e892dd6c1f64faf85bd5a1ed53f2be468
chendeshan <1030901952@qq.com>
2020-03-18 20:26:28 +0800

    增加失败弹窗。

--- app/src/main/java/com/example/test3/view/activity/UpgradeActivity.java
+++ app/src/main/java/com/example/test3/view/activity/UpgradeActivity.java
@@ -1,10 +1,12 @@
 package com.example.test3.view.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.test3.R;
+import com.example.test3.view.widget.BaseAlertDialog;
 
 public class UpgradeActivity extends Activity {
 
@@ -15,9 +17,32 @@ public class UpgradeActivity extends Activity {
     }
 
     public void netUpgradeClick(View view) {
+        showFailDialog();
     }
 
     public void manualUpgradeClick(View view) {
 
+    }
+
+    public void showFailDialog() {
+        BaseAlertDialog dialog = new BaseAlertDialog.Builder(this)
+                .setTitle("升级失败了")
+                .setMessage("是否重新升级")
+                .setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("再想想", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelEnable(false)
+                .build();
+
+        dialog.show();
     }
 }
