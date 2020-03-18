a8e4a3044925efe55fc959b876fa8c0c767893ee
chendeshan <1030901952@qq.com>
2020-03-18 17:49:13 +0800

    简单实用教程。

--- app/src/main/java/com/example/test3/activity/AdjustCameraActivity.java
+++ app/src/main/java/com/example/test3/activity/AdjustCameraActivity.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.test3.R;
 
-public class AdjustCameraActivity extends Activity {
+public class AdjustCameraActivity extends Activity implements View.OnClickListener, View.OnFocusChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adjust_camera);
+        Button button1 = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(this);
+//        button.requestFocus();
+//        button.setOnFocusChangeListener(this);
+//        button2.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        v.id
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+//        switch (v.getId()){
+//            case buttnId:
+//                break;
+//        }
     }
 }
