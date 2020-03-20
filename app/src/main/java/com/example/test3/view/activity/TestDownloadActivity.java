a35c1a1905dee955d311f21b97a3d4709dd265ab
chendeshan <1030901952@qq.com>
2020-03-20 09:24:05 +0800

    增加下载调试页。

---
+++ app/src/main/java/com/example/test3/view/activity/TestDownloadActivity.java
@@ -0,0 +1,16 @@
+package com.example.test3.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test3.R;
+
+public class TestDownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_download);
+    }
+}
