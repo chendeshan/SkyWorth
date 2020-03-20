db0c7a5fa0ccbee6292e508e675395407654c7e9
chendeshan <1030901952@qq.com>
2020-03-20 13:35:29 +0800

    增加获取文件md5的方法。

--- app/src/main/java/com/example/test3/urils/CommonUtil.java
+++ app/src/main/java/com/example/test3/urils/CommonUtil.java
@@ -1,5 +1,8 @@
 package com.example.test3.urils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 
 public class CommonUtil {
@@ -24,5 +27,60 @@ public class CommonUtil {
         } catch (Exception e) {
             return null;
         }
+    }
+
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+
+        MessageDigest digest;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+
+        try {
+
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return bytesToHexString(digest.digest());
+    }
+
+    public static String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+
+        for (int i = 0; i < src.length; i++) {
+            int v = src[i] & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+
+        return stringBuilder.toString();
     }
 }
