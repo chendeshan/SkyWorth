5a01c2bcb8d6d7bd394a4a3e9a9aa12954e0bfd1
chenjc <chenjincai@cardioray.com.cn>
2020-03-22 16:30:27 +0800

    基类中增加Toast方法。

--- app/src/main/java/com/example/test3/view/activity/BaseActivity.java
+++ app/src/main/java/com/example/test3/view/activity/BaseActivity.java
@@ -3,6 +3,12 @@ package com.example.test3.view.activity;
 import android.app.Activity;
 
 public class BaseActivity extends Activity {
+    public void showToast(int msgId) {
+        showToast(getString(msgId));
+    }
 
+    public void showToast(String msg) {
+        ToastUtil.showToast(this, msg);
+    }
 
 }
