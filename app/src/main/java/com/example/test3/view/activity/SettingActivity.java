7db71e1a771d4004de8e48c45ebf20738d3be274
chendeshan <1030901952@qq.com>
2020-03-23 09:54:43 +0800

    增加版本更新方法。

--- app/src/main/java/com/example/test3/view/activity/SettingActivity.java
+++ app/src/main/java/com/example/test3/view/activity/SettingActivity.java
@@ -4,16 +4,23 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.test3.R;
 
 public class SettingActivity extends BaseActivity {
 
+    private TextView mVersionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        initView();
+    }
 
+    private void initView() {
+        mVersionTextView = (TextView) findViewById(R.id.activity_setting_version_text);
     }
 
     @Override
@@ -22,6 +29,11 @@ public class SettingActivity extends BaseActivity {
         setPermissionGroup(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE});
     }
 
+    public void updateVersion(String text) {
+        mVersionTextView.setText(text);
+    }
+
+    /*click*/
     public void upgradeClick(View view) {
         Intent intent = new Intent(this, UpgradeActivity.class);
 
